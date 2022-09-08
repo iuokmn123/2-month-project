e3b86a778a156c5d53d633f4edf7f7440e5b5bf3
iuokmn123 <110828254+iuokmn123@users.noreply.github.com>
2022-09-08 22:05:51 +0630

    summary
    this is new fixing

---
+++ js/questions.js
@@ -0,0 +1,69 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What would you do if it ________ on your wedding day?:",
+    answer: "rained",
+    options: [
+      "rained",
+      "will rain",
+      "would rain",
+      "should rain",
+    ],
+  },
+  {
+    numb: 2,
+    question: " If she comes, I _____ call you.",
+    answer: "will",
+    options: [
+      "will",
+      "would",
+      "would have",
+      "have",
+    ],
+  },
+  {
+    numb: 3,
+    question: " If I eat peanut butter, I ________ sick.",
+    answer: "get",
+    options: [
+      "would have gotten",
+      "would get",
+      "get",
+      "got",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What will you do if you ________ the history exam?",
+    answer: "fail",
+    options: [
+      "would fail",
+      "will fail",
+      "fail",
+      "failed",
+    ],
+  },
+  {
+    numb: 5,
+    question: "If they had not _____ the car, I would have driven you.",
+    answer: "taken",
+    options: [
+      "take",
+      "taken",
+      "would take",
+      "took",
+    ],
+  },
+  {
+   numb: 6,
+   question: "If it snows, ________ still drive to the coast?",
+   answer: "will you",
+   options: [
+      "will you",
+      "would you",
+      "would you have",
+      "have",
+   ]
+  },
+];
